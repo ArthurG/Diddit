d972156cd5421c75f52c9c8ef301e7c97fed209e
Harman Singh <harman.j.singh@hotmail.com>
2017-01-21 14:52:40 -0600

    Init React Client

---
+++ DidItClient/src/components/layouts/login-layout.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+class Login extends React.Component {
+  
+  fart() {
+      return "poop";
+  }  
+
+
+  render() {
+    return(
+
+        <h1> poop </h1>
+    );
+    
+ }
+
+
+}
+
+export default Login;
---
+++ DidItClient/src/router.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Router, Route, browserHistory, IndexRoute} from 'react-router';
+import {App} from './App';
+import { Login } from './components/layouts/login-layout.jsx';
+
+export default(
+    <Router history={browserHistory}>
+    
+    </Router>
+
+);
