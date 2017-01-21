0c792ea60d1ffb5a6294aa2bee8de480f4214412
Arthur Guo <arthur.guo@uwaterloo.ca>
2017-01-21 09:20:58 -0500

    Add user model

--- backend/diddit/__init__.py
+++ backend/diddit/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from .webhook import webhook
-from .profile import profile
+from .customer import customer
 
 diddit = Flask (__name__)
 diddit.register_blueprint(webhook)
-diddit.register_blueprint(profile)
+diddit.register_blueprint(customer)
 
 db = SQLAlchemy(diddit)
--- backend/diddit/models.py
+++ backend/diddit/models.py
@@ -1,9 +1,24 @@
 from flask import Flask
+import json
 from flask_sqlalchemy import SQLAlchemy
-from app import db
-from . import db
+from diddit import db
+import requests
 
 class User(db.Model):
-    uid = db.Column(db.String(128), nullable=False, unique=True)
-    password = db.Column(db.String(192), nullable = False)
-    cron_news = db.Column(db.boolean(192), nullable = False)
+    id = db.Column(db.Integer, nullable=False, unique=True, primary_key=True)
+    location = db.Column(db.String(500), nullable = False)
+    lat = db.Column(db.Float(500), nullable = True)
+    lng = db.Column(db.Float(500), nullable = True)
+    username = db.Column(db.String(500), nullable = False)
+    password = db.Column(db.String(500), nullable = False)
+    def __init__(self, location, username, password):
+        self.location = location
+        data={'address':location}
+        endpoint="https://maps.googleapis.com/maps/api/geocode/json"
+        resp = requests.get(endpoint, params=data)
+        latlng = json.loads(resp.text)['results'][0]['geometry']['location']
+        self.lat = latlng['lat']
+        self.lng = latlng['lng']
+        self.username = username
+        self.password = password
+db.create_all()
