447454b36cbab182f07a27e077c102b2de85a8e3
Vaibhav <ishuvaibhav@gmail.com>
2017-01-21 11:09:56 -0500

    making endpoints

--- backend/diddit/customer/views.py
+++ backend/diddit/customer/views.py
@@ -2,6 +2,11 @@ from flask import request
 from . import webhook
 import sys
 from . import message_sender
+sys.path.append('../models.py')
+from models import User
+from flask_sqlalchemy import SQLAlchemy
+import json 
+import requests
 
 
 @webhook.route('/webhook', methods=['GET'])
@@ -17,22 +22,50 @@ def verify():
 
 
 
-@webhook.route('/webhook', methods=['POST'])
-def processWebhook():
+@webhook.route('/login', methods=['GET'])
+def login():
 
+	# endpoint for processing incoming messaging events
+    data = request.get_json()
+    log("incoming msg " + str(data))
+
+    userName = data['username']
+    passWord = data['password']
+
+    Users = User.query.all().filter(User.username == userName)
+
+    for aUser in Users:
+    	if aUser.password == passWord:
+    		return "ok", 200
+
+    return "unauthorized", 401 
+
+
+
+@webhook.route('/signup', methods=['POST'])
+def signup():
     # endpoint for processing incoming messaging events
-
     data = request.get_json()
     log("incoming msg " + str(data))  # you may not want to log every incoming message in production, but it's good for testing
 
-    if data["object"] == "page":
+    # endpoint for signups
+    if ('location' in data) and ('storename' in data) and ('username' in data) and ('password' in data):
+    	location = data['location']
+    	storename = data['storename']
+    	userName = data['username']
+    	password = data['password']
+    	'''
+    	Users = User.query.all().filter(User.username == userName)
+    		for aUser in Users:
+    			if (aUser.username == userName) and (aUser.storename == storename):
+    				return "user already registered", 400
+		'''
+    	temp = User(location, storename, userName, password)
+    	db.session.add(temp)
+    	db.session.commit()
 
-        for entry in data["entry"]:
-            for messaging_event in entry["messaging"]:
-                message_sender.text_message(messaging_event['sender']['id'], messaging_event['message']['text'])
-                print(messaging_event['sender']['id'], messaging_event['message']['text'])
-                #message_controller.route(messaging_event)
     return "ok", 200
+
 
 def log(message):  # simple wrapper for logging to stdout on heroku
     print(str(message))
