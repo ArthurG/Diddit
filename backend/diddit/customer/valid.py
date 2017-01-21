00201fb78351ad9e81f84cdf42c666e013e53886
Vaibhav <ishuvaibhav@gmail.com>
2017-01-21 09:07:20 -0500

    finished validation of questionType

--- backend/diddit/customer/valid.py
+++ backend/diddit/customer/valid.py
@@ -1,6 +1,7 @@
 # m - mc
 # s - short answer 
 # r - rate 1- 10 
+import sys 
 
 possibleRates = list(range(1,11))
 rates = map( str ,possibleRates )
@@ -10,33 +11,49 @@ possibleMC = [ "a", "b", "c", "d", "A", "B", "C", "D" ]
 
 def DetectSpamForRate( answer ):
 	if not(answer in rates): 
-		return false
+		return False
 	else:
-		return true 
+		return True 
 
 
 def DetectSpamForMC( answer ):
 	if not(answer in possibleMC):
-		return false
+		return False
 	else:
-		return true
+		return True
 
 
 def DetectSpamForSA( answer ):
 	if len(answer) > 255:
-		return false
+		return False
 	else:
-		return true 
+		return True 
 
 
-if (questionType = "m"):
-	DetectSpamForMC(response)
+def main(response, questionType):
+
+	if (questionType == "m"):
+		x = DetectSpamForMC(response)
+		return x
+
+	if (questionType == "s"):
+		y = DetectSpamForSA(response)
+		return y
+
+	if (questionType == "r"):
+		z = DetectSpamForRate(response)
+		return z
 
 
-if (questionType = "s"):
-	DetectSpamForSA(response)
 
 
-if (questionType = "r"):
-	DetectSpamForRate(response)
+if __name__ == "__main__":
+    # execute only if run as a script
+    argsLen = len(sys.argv)
+    response = sys.argv[1]
+    questionType = sys.argv[2]
 
+    if (argsLen > 2):
+    	yo = main(response , questionType)
+    	print yo
+    	
