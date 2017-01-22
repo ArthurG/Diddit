4c9e9543e45f2ba7f34ed62bad6d8688d5b8440a
Harman Singh <harman.j.singh@hotmail.com>
2017-01-21 20:59:18 -0600

    CHloe add style

--- DidItClient/src/index.js
+++ DidItClient/src/index.js
@@ -4,7 +4,10 @@ import App from './App';
 
 import Login from './components/layouts/login-layout.jsx';
 import Signup from './components/layouts/signup-layout.jsx'
+import SurveyContainer from './components/containers/survey-container.jsx';
+
+
 
 import Router from './router.jsx';
 
-ReactDOM.render( < Signup />, document.getElementById('root') );
+ReactDOM.render( < SurveyContainer />, document.getElementById('root') );
--- DidItClient/src/router.jsx
+++ DidItClient/src/router.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Router, Route, browserHistory, IndexRoute} from 'react-router';
 import {App} from './App';
 import { Login } from './components/layouts/login-layout.jsx';
+import { SurveyContainer } from './'
 
 export default(
     <Router history={browserHistory}>
